7e5ce6fca482d007581ce3e9933da97fa029ffbf
John Anderson <sontek@gmail.com>
2010-01-07 18:42:05 -0600

    pull the objects

--- urls.py
+++ urls.py
@@ -3,7 +3,7 @@ from tekblog.models import Entry
 from datetime import datetime
 
 active_entries = {
-    'queryset': Entry.active_objects,
+    'queryset': Entry.active_objects.all(),
 }
 
 urlpatterns = patterns('django.views.generic.list_detail',
