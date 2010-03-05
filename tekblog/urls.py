ad3304e6024afe77e469aa93af5c59db7b23153a
John Anderson <sontek@gmail.com>
2010-03-05 10:17:01 -0600

    added url support for blogengine.net

--- tekblog/urls.py
+++ tekblog/urls.py
@@ -5,5 +5,7 @@ from tekblog.views import index, detail
 urlpatterns = patterns('tekblog.views',
         url(r'^$', index, name='tekblog_index'),
         url(r'^(?P<slug>[^/]+)$', detail, name='tekblog_detail'),
+        # support blogengine.net url's
+        url(r'^post/(?P<slug>[^/]+).aspx$', detail, name='tekblog_detail'),
         (r'^comments/', include('django.contrib.comments.urls')),
 )
