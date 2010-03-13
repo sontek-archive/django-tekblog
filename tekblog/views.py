09fd8e651bbf7645c99533cfa1b5a112cb102eee
John Anderson <sontek@gmail.com>
2010-03-13 12:39:48 -0700

    added paging support

--- tekblog/views.py
+++ tekblog/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import get_object_or_404, render_to_response
 from django.template import RequestContext
 from tekblog.models import Entry
+from django.core.paginator import Paginator
 
-def index(request):
-    active = Entry.objects.active()
-    return render_to_response('tekblog/index.html', {'entries': active},
+def index(request, page=1):
+    paginator = Paginator(Entry.objects.active(), 5)
+    pager = paginator.page(page)
+    return render_to_response('tekblog/index.html', {'pager': pager},
             context_instance=RequestContext(request))
 
 def detail(request, slug):
