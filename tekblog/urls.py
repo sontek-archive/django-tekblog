8769f1cde06d7a50c13e98849a5896e601d56deb
John Anderson <sontek@gmail.com>
2010-04-25 21:23:19 +0000

    support parentheses in slug

--- tekblog/urls.py
+++ tekblog/urls.py
@@ -5,8 +5,8 @@ from tekblog.views import index, detail
 urlpatterns = patterns('tekblog.views',
         url(r'^$', index, name='tekblog_index'),
         url(r'^(?P<page>\d*)/?$', index, name='tekblog_index'),
-        url(r'^(?P<slug>[-\w]+)/?$', detail, name='tekblog_detail'),
+        url(r'^(?P<slug>[-\w\(\)]+)/?$', detail, name='tekblog_detail'),
         # support blogengine.net url's
-        url(r'^post/(?P<slug>[-\w]+).aspx$', detail, name='tekblog_detail_be'),
+        url(r'^post/(?P<slug>[-\w\(\)]+).aspx$', detail, name='tekblog_detail_be'),
         (r'^comments/', include('django.contrib.comments.urls')),
 )
