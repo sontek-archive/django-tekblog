5ed41b578b00eeda14e46526e4fa5c794f88a322
Thomas Holloway <nyxtom@gmail.com>
2010-06-11 08:47:16 -0500

    Updated to use template= parameter so they can be overriden easily from urls

--- tekblog/views.py
+++ tekblog/views.py
@@ -3,15 +3,14 @@ from django.template import RequestContext
 from tekblog.models import Entry
 from django.core.paginator import Paginator
 
-def index(request, page=1):
+def index(request, page=1, template='tekblog/index.html'):
     paginator = Paginator(Entry.objects.active(), 5)
     pager = paginator.page(page)
-    return render_to_response('tekblog/index.html', {'pager': pager},
+    return render_to_response(template, {'pager': pager},
             context_instance=RequestContext(request))
 
-def detail(request, slug):
+def detail(request, slug, template='tekblog/detail.html'):
     entry = get_object_or_404(Entry, slug=slug)
 
-    return render_to_response('tekblog/detail.html', {'entry': entry},
+    return render_to_response(template, {'entry': entry},
             context_instance=RequestContext(request))
-    
