312af273850377be3fdabcedf90a9168c93d7f63
John Anderson <sontek@gmail.com>
2010-06-12 09:26:17 +0800

    include a link element

--- tekblog/feeds.py
+++ tekblog/feeds.py
@@ -5,8 +5,11 @@ from tekblog.models import Entry, Series
 
 class LatestEntriesFeed(Feed):
     _site = Site.objects.get_current()
+    link = '/'
     title = '%s feed' % _site.name
     description = '%s latest posts feed.' % _site.name
 
     def items(self):
+        import pdb
+        pdb.set_trace()
         return Entry.objects.active()
