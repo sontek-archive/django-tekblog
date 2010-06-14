7170af5f5ec40f0947a3eefe08ab6d6c0124f157
John Anderson <sontek@gmail.com>
2010-06-14 08:17:14 +0800

    wrong namespace

--- tekblog/settings.py
+++ tekblog/settings.py
@@ -1,4 +1,4 @@
-HAYSTACK_SITECONF = 'tekpress.search_sites'
+HAYSTACK_SITECONF = 'tekblog.search_sites'
 HAYSTACK_SEARCH_ENGINE = 'whoosh'
 HAYSTACK_WHOOSH_PATH = os.path.join(PROJECT_ROOT, 'haystack')
 COMMENTS_APP='tekrecaptcha'
