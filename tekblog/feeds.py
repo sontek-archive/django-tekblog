8f7b29b549ad01dd80296fe816272b85d858d515
John Anderson <sontek@gmail.com>
2010-12-13 01:28:01 -0600

    Created a topic ress feed

--- tekblog/feeds.py
+++ tekblog/feeds.py
@@ -2,6 +2,8 @@ from django.conf import settings
 from django.contrib.sites.models import Site
 from django.contrib.syndication.views import Feed
 from tekblog.models import Entry, Series
+from tagging.models import TaggedItem, Tag
+
 
 class LatestEntriesFeed(Feed):
     _site = Site.objects.get_current()
@@ -11,7 +13,26 @@ class LatestEntriesFeed(Feed):
 
     def items(self):
         return Entry.objects.active()
-    
+
+    def item_title(self, item):
+        return item.title
+
+    def item_description(self, item):
+        return item.html_content
+
+
+class TopicFeed(LatestEntriesFeed):
+    def get_object(self, request, topic):
+        t = Tag.objects.get(name=topic)
+        return t
+
+    def items(self, topic):
+        # need to do this so django-tagging will allow muliple words in a tag
+        cleaned_topic = '"%s"' % (topic.name)
+        active_entries = TaggedItem.objects.get_by_model(
+                Entry.objects.active(), cleaned_topic)
+        return active_entries
+
     def item_title(self, item):
         return item.title
 
--- tekblog/urls.py
+++ tekblog/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls.defaults import *
 from datetime import datetime
 from tekblog.views import index, detail, search
-from tekblog.feeds import LatestEntriesFeed 
+from tekblog.feeds import LatestEntriesFeed, TopicFeed 
 
 urlpatterns = patterns('tekblog.views',
         url(r'^$', index, name='tekblog_index'),
@@ -13,4 +13,5 @@ urlpatterns = patterns('tekblog.views',
         url(r'^post/(?P<slug>[^/]*).aspx$', detail, name='tekblog_detail_be'),
         (r'^comments/', include('django.contrib.comments.urls')),
         (r'^feeds/latest/$', LatestEntriesFeed()),
+        (r'^feeds/(?P<topic>[^/]*)/$', TopicFeed()),
 )
