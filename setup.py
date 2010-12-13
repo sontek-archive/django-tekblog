93031d8be19d256f9f800d74eb736643f697dbdd
John Anderson <sontek@gmail.com>
2010-12-12 22:30:03 -0600

    added a setup file and updated .gitignore

---
+++ setup.py
@@ -0,0 +1,23 @@
+try: 
+    from setuptools import setup
+except ImportError: 
+    from distutils.core import setup 
+
+setup(
+    name='django-tekblog',
+    version=__import__('tekblog').__version__,
+    description='A django blog engine',
+    long_description='',
+    author='John Anderson',
+    author_email='sontek@gmail.com',
+    url='http://github.com/sontek/django-tekblog',
+    download_url='http://github.com/sontek/django-tekblog',
+    license='BSD',
+    packages=['tekblog'],
+    classifiers = [
+            'Framework :: Django',
+            'License :: OSI Approved :: BSD License',
+            'Operating System :: OS Independent',
+            'Programming Language :: Python',
+    ],
+)
