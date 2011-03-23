c628bf9371ac60061c48e6526dd1d23b54dc7348
John Anderson <sontek@gmail.com>
2011-03-23 14:17:36 -0500

    remove code blocks before we markdown/textile them

--- tekblog/utils.py
+++ tekblog/utils.py
@@ -27,6 +27,7 @@ class Formatter:
     def format(self, format, text):
         index = 0
         last_index = 0
+        escaped_blocks = []
 
         while index >= 0:
             index = text.find('<code', last_index)
@@ -38,16 +39,18 @@ class Formatter:
             last_index = text.find('</code>', mid_index)
             code_to_esc = text[mid_index+1:last_index]
 
-            lang_index = text[index:mid_index+1].index('class=')
+            language = None
+
+            lang_index = text[index:mid_index+1].find('class=')
+
             if lang_index > 0:
                 language = text[index:mid_index+1][lang_index+7:mid_index-1]
-            else:
-                langauge = None
 
             if language:
                 try:
                     lexer = get_lexer_by_name(language, stripnl=True,
                             encoding='UTF-8')
+
                 except ValueError, e:
                     try:
                         lexer = guess_lexer(code_to_esc)
@@ -63,10 +66,14 @@ class Formatter:
 
             formatter = HtmlFormatter(linenos=False, cssclass='source')
             escaped_code = highlight(code_to_esc, lexer, formatter)
-
-            text = text[0:mid_index+1] + escaped_code + text[mid_index+len(code_to_esc):]
+            escaped_blocks.append(text[0:mid_index] + escaped_code + text[mid_index+len(code_to_esc):])
 
         soup = BeautifulSoup(text)
+        code_blocks = soup.findAll(u'code')
+
+        # Put place holder blocks
+        for block in code_blocks:
+            block.replaceWith(u'<code class="removed"></code>')
 
         if format == 'none':
             text = str(soup)
@@ -78,6 +85,7 @@ class Formatter:
             text = self.markdown(str(soup))
 
         soup = BeautifulSoup(text)
+        empty_code_blocks = soup.findAll('code', 'removed')
 
         # Find all instances of img tokens where the class has
         # replace-media-url replace all {{ MEDIA_URL }} tokens within the
@@ -93,6 +101,10 @@ class Formatter:
                 image['src'] = image['src'].replace(
                         '{{ MEDIA_URL }}', settings.MEDIA_URL)
 
+        for index, escaped_block in enumerate(escaped_blocks):
+            empty_code_blocks[index].replaceWith(escaped_block)
+
+        import pdb;pdb.set_trace()
         return str(soup)
 
 
