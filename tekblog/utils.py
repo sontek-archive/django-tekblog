25b7fefec52dd985ec3bc554cb2e1b25c8f680dd
John Anderson <sontek@gmail.com>
2011-03-23 03:42:56 -0500

    updated syntax highlighting parser to html escape the content

--- tekblog/utils.py
+++ tekblog/utils.py
@@ -15,7 +15,6 @@ from pygments.lexers import get_lexer_by_name, guess_lexer
 from BeautifulSoup import BeautifulSoup
 from django.conf import settings
 
-
 class Formatter:
     # The types of markup that are available
     MARKUP_CHOICES = (
@@ -26,12 +25,49 @@ class Formatter:
     )
 
     def format(self, format, text):
-        soup = BeautifulSoup(text)
-        code_blocks = soup.findAll(u'code')
+        index = 0
+        last_index = 0
 
-        # Put place holder blocks
-        for block in code_blocks:
-            block.replaceWith(u'<code class="removed"></code>')
+        while index >= 0:
+            index = text.find('<code', last_index)
+
+            if index < 0:
+                break
+
+            mid_index = text.find('>', index)
+            last_index = text.find('</code>', mid_index)
+            code_to_esc = text[mid_index+1:last_index]
+
+            lang_index = text[index:mid_index+1].index('class=')
+            if lang_index > 0:
+                language = text[index:mid_index+1][lang_index+7:mid_index-1]
+            else:
+                langauge = None
+
+            if language:
+                try:
+                    lexer = get_lexer_by_name(language, stripnl=True,
+                            encoding='UTF-8')
+                except ValueError, e:
+                    try:
+                        lexer = guess_lexer(code_to_esc)
+                    except ValueError, e:
+                        lexer = get_lexer_by_name('text', stripnl=True,
+                                encoding='UTF-8')
+            else:
+                try:
+                    lexer = guess_lexer(code_to_esc)
+                except ValueError, e:
+                    lexer = get_lexer_by_name('text', stripnl=True,
+                            encoding='UTF-8')
+
+            formatter = HtmlFormatter(linenos=False, cssclass='source')
+            escaped_code = highlight(code_to_esc, lexer, formatter)
+
+            import pdb;pdb.set_trace()
+            text = text[0:mid_index+1] + escaped_code + text[mid_index+len(code_to_esc):]
+
+        soup = BeautifulSoup(text)
 
         if format == 'none':
             text = str(soup)
@@ -51,44 +87,15 @@ class Formatter:
                 'class':
                 re.compile(r'.+replace-media-url')
             })
+
         for image in images:
             if 'src' in image and image['src'].startswith(
                     '{{ MEDIA_URL }}'):
                 image['src'] = image['src'].replace(
                         '{{ MEDIA_URL }}', settings.MEDIA_URL)
 
-        index = 0
-        empty_code_blocks = soup.findAll('code', 'removed')
-        formatter = HtmlFormatter(linenos=False, cssclass='source')
-
-        language = None
-        for block in code_blocks:
-            if block.has_key('class'):
-                language = block['class']
-
-            if language:
-                try:
-                    lexer = get_lexer_by_name(language, stripnl=True,
-                            encoding='UTF-8')
-                except ValueError, e:
-                    try:
-                        lexer = guess_lexer(block.renderContents())
-                    except ValueError, e:
-                        lexer = get_lexer_by_name('text', stripnl=True,
-                                encoding='UTF-8')
-            else:
-                try:
-                    lexer = guess_lexer(block.renderContents())
-                except ValueError, e:
-                    lexer = get_lexer_by_name('text', stripnl=True,
-                            encoding='UTF-8')
-
-            empty_code_blocks[index].replaceWith(
-                    highlight(block.renderContents(), lexer, formatter))
-
-            index = index + 1
-
         return str(soup)
+
 
     def markdown(self, text):
         return markdown.markdown(text)
