67c969750608620da876c38cbfe78af6742effce
John Anderson <sontek@gmail.com>
2011-03-25 00:15:55 -0500

    added tekblog urls

--- tekblog_sample/urls.py
+++ tekblog_sample/urls.py
@@ -3,13 +3,6 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'tekblog_sample.views.home', name='home'),
-    # url(r'^tekblog_sample/', include('tekblog_sample.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
+    url(r'', include('tekblog.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
